3c3c33b882a300cc878a90c702eafbafafb88a77
dermo <dermot.logue@dkit.ie>
2023-10-24 13:28:35 +0100

    added second line

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Git Github and Branching");
+        System.out.println("Line 2");
     }
 }
