cc7d97ee762861f316fe0f55cf1ec662af0914f9
dermo <dermot.logue@dkit.ie>
2023-10-25 14:24:48 +0100

    my other change

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,6 +14,7 @@ public class Main {
         System.out.println("SD2a - change 1");
         System.out.println("My change ");
         System.out.println("buddies changes");
+        System.out.println("my other change");
 
     }
 }
